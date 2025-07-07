f4bb4e2618626370d9f8fcf6bfaeab89b3f9f34c
Daffa Althaf Ridovani <daffaalthaf25@gmail.com>
2025-07-07 09:51:05 +0700

    chore: comment out unused Features and Footer imports in HomePage component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Features from "@/components/home/Features";
-import Footer from "@/components/home/Footer";
+// import Features from "@/components/home/Features";
+// import Footer from "@/components/home/Footer";
 import Hero from "@/components/home/Hero";
 import Navbar from "@/components/home/Navbar";
 
